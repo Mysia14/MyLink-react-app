04e95173fe63279c3c6d4f46b80987bf985b2fa0
github_mysia14 <mysiasanta@gmail.com>
2022-02-09 11:48:34 -0700

    added FiLink input

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,21 @@
+import {FiLink} from "react-icons/fi";
+
 export default function Home(){
     return (
     <div className="container-home">
+
     <div className="logo">
       <img src="./assets/Logo.png" alt=" Subject Link logo"></img>
       <h1>Your Links</h1>
       <span>✨ Paste your link here ✨</span>
     </div>
+
+    <div className="area-input">
+      <div>
+        <FiLink size={24} color="#FFF" />
+        <input placeholder="Paste your link here ..."/>
+    </div>  
+    </div>
     </div>
   )
   }
-  
-  
